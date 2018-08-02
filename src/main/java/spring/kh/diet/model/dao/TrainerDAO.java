be3ed07233710fa003508712a928db335c1cbb65
AHJ <savior0319@naver.com>
2018-08-02 15:31:43 +0900

    트레이너 찾기 완료(뷰만들어야함)

--- src/main/java/spring/kh/diet/model/dao/TrainerDAO.java
+++ src/main/java/spring/kh/diet/model/dao/TrainerDAO.java
@@ -1,10 +1,12 @@
 package spring.kh.diet.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 import spring.kh.diet.model.vo.MemberVO;
 import spring.kh.diet.model.vo.TrainerProgramVO;
-
+import spring.kh.diet.model.vo.TrainerSearchVO;
 import spring.kh.diet.model.vo.TrainingRegVO;
 
 public interface TrainerDAO {
@@ -14,5 +16,7 @@ public interface TrainerDAO {
 	int registTrainerProgram(SqlSessionTemplate session, TrainerProgramVO tpv);
 
 	int trainerReg(SqlSessionTemplate session, TrainingRegVO register);
+
+	ArrayList<TrainingRegVO> trainerReg(SqlSessionTemplate session, TrainerSearchVO tsv);
 
 }
