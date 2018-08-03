525530aaf15351abda8277d4d4dc546c47a7d9ec
parkhyeyoon <39255234+parkhyeyoon@users.noreply.github.com>
2018-08-03 18:25:09 +0900

    마이페이지 - 트레이너 업데이트

--- src/main/java/spring/kh/diet/model/dao/MyInfoDAO.java
+++ src/main/java/spring/kh/diet/model/dao/MyInfoDAO.java
@@ -10,6 +10,7 @@ import spring.kh.diet.model.vo.BoardPostVO;
 import spring.kh.diet.model.vo.MemberVO;
 import spring.kh.diet.model.vo.MyActivityVO;
 import spring.kh.diet.model.vo.OneSessionVO;
+import spring.kh.diet.model.vo.PaymentPDVO;
 import spring.kh.diet.model.vo.PaymentVO;
 import spring.kh.diet.model.vo.QuestionVO;
 import spring.kh.diet.model.vo.TrainerProgramVO;
